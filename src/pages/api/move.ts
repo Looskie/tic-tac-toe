9cf3832526664da45661bcde748ab72f202ce341
Cody Miller <hello@looskie.com>
2023-02-26 12:20:01 -0500

    fix: checkWinner

--- src/pages/api/move.ts
+++ src/pages/api/move.ts
@@ -74,27 +74,46 @@ export default async function handler(
   res.status(204);
 }
 
-function transpose(matrix: string[][]) {
-  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
-}
-
-function checkWinner(board: string[][]): string | null {
-  for (const row of board) {
-    if (row.slice(1).every((x) => x === row[0])) {
-      return row[0];
+const checkWinner = (board: string[][]): string | null => {
+  // Check rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2] &&
+      board[i][0] !== null
+    ) {
+      return board[i][0];
     }
   }
 
-  const flipped = transpose(board);
-
-  for (const column of flipped) {
-    if (column.slice(1).every((x) => x === column[0])) {
-      return column[0];
+  // Check columns
+  for (let j = 0; j < 3; j++) {
+    if (
+      board[0][j] === board[1][j] &&
+      board[1][j] === board[2][j] &&
+      board[0][j] !== null
+    ) {
+      return board[0][j];
     }
   }
 
-  const diagOne = board[0][0] === board[1][1] && board[1][1] === board[2][2];
-  const diagTwo = board[2][0] === board[1][1] && board[1][1] === board[0][2];
+  // Check diagonals
+  if (
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2] &&
+    board[0][0] !== null
+  ) {
+    return board[0][0];
+  }
 
-  return diagOne ? board[0][0] : diagTwo ? board[2][0] : null;
-}
+  if (
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0] &&
+    board[0][2] !== null
+  ) {
+    return board[0][2];
+  }
+
+  // If there is no winner, return null
+  return null;
+};
