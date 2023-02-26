0880965a2a559e39bb538f6e76ed332738a5306e
Cody Miller <hello@looskie.com>
2023-02-26 11:55:02 -0500

    feat: cleanup previous games if they werent finished

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { GameState } from "../../types";
+import { PROJECT_ID } from "../../utils/Commons";
+import { hop } from "../../utils/Hop";
+
+const ONE_HOUR = 60 * 60 * 1000;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.key !== process.env.SUPER_SECRET_CRON_SECRET) {
+    res.status(404).end();
+    return;
+  }
+  // cleans up games that are older than an hour
+  const channels = await hop.channels.getAll(PROJECT_ID).catch(() => []);
+
+  for (const channel of channels) {
+    const channelState = channel.state as unknown as GameState;
+
+    console.log(lessThanOneHourAgo(new Date(channelState.created_at)));
+    if (lessThanOneHourAgo(new Date(channelState.created_at))) {
+      continue;
+    }
+
+    hop.channels.delete(channel.id);
+  }
+
+  res.status(200).end("Hello Cron!");
+}
+
+const lessThanOneHourAgo = (date: Date) => {
+  return new Date().getTime() - date.getTime() < ONE_HOUR;
+};
